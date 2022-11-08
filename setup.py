55e75356d5cdea929324aeedc6db5b70cbbecd2b
nickseek <nickseek@users.noreply.github.com>
2022-11-08 15:56:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='q_squared',
+setup(name='q-squared',
       version='0.1',
       description='A reference-free metric that aims to evaluate the factual consistency of knowledge-grounded dialogue systems',
       url='https://github.com/nickseek/q-squared/tree/split_long',
       author='Or Honovich',
       author_email='or.honovich@gmail.com',
       license='Apache License 2.0',
-      packages=['pipeline',
+      packages=['q-squared',
                 ],
       install_requires=[
           'numpy', 'pandas','bert-score','spacy','torch','transformers','sentencepiece', 'protobuf==3.20'
